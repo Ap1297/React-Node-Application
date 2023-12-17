595a33e48607e4784eafa063614f546e85e77e72
Ankit <40174133+Ap1297@users.noreply.github.com>
2023-12-17 13:29:57 +0530

    UseEffect example

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,23 @@
 import Header from "./header";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 const App = () => {
 
     //Example of state and event handler
     const [counter,setCounter] = useState(0);
 
+    //Example of useEffect
+    useEffect(() => {
+       const intervalId = setInterval(() => {
+            setCounter(counter+1);
+        }, 1000);
+
+        //Clean up function
+        return () => {
+            clearInterval(intervalId);
+        };
+    })
+
+    
     return( 
     <div className="container" >
         <Header message = "Naming Contest"/>
